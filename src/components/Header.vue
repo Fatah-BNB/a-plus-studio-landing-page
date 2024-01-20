9d6cc65c8fbd197fecb76c9034e876462584d34d
Abdelfatah <bnb.fatah@gmail.com>
2024-01-20 11:02:47 +0100

    Fixes: header spacing

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="md:mx-[8%] md:my-11">
-    <header id="top-menu" class="flex flex-row justify-between md:items-center">
+    <header id="top-menu" class="flex flex-row justify-between">
       <div class="w-[10%] lg:w-[7%] md:w-[3%] flex flex-row md:justify-center md:py-0 py-5 md:px-0 px-10">
         <span style="font-family: nova;" class="lg:text-[1.5rem] md:text-[1.3rem] md:pr-1 text-[2rem]">A+</span><span
           class="lg:text-2xl md:text-xl md:pl-1 leading-normal md:leading-2 text-3xl">Studio</span>
@@ -8,7 +8,7 @@
       <nav id="nav-options relative my-10">
         <ul
           :class="{ 'translate-x-0 translate-y-[25%]': menuToggle, '-translate-x-[100%] translate-y-[25%] md:translate-y-0': !menuToggle, }"
-          class="py-5 bg-white left-0 md:translate-x-0 flex md:flex-row  md:h-full z-10 md:static absolute flex-col w-full items-center space-y-6 md:space-y-0 transition-transform duration-300 ease-in-out">
+          class="py-5 md:py-0 bg-white left-0 md:translate-x-0 flex md:flex-row md:h-full z-10 md:static absolute flex-col w-full items-center space-y-6 md:space-y-0 transition-transform duration-300 ease-in-out">
           <li v-for="route in routes" :key="route.name" @click="toggleMenu"
             class="hover:bg-black 2xl:px-12 lg:px-8 md:px-5 hover:text-white transition-all text-base font-extrabold flex md:items-center">
             <router-link :to="route.path">{{ route.name }}</router-link>
